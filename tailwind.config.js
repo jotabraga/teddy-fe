8177b695adabce478f9153c12901454272b4c282
jotabraga <jpmbraga.feg@gmail.com>
2024-11-09 21:26:03 -0300

    feat: first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Inclui arquivos TypeScript e React (.tsx)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
