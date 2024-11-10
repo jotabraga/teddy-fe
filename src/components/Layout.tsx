6974c3c7c925d017eeaa9bbdcaf5169e505e423b
jotabraga <jpmbraga.feg@gmail.com>
2024-11-10 15:10:41 -0300

    feat: create layout structure

---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import { AppSidebar } from "@/components/layout/app-sidebar";
+import Header from "@/components/layout/header";
+import { SidebarProvider } from "@/components/ui/sidebar";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <div className="flex w-full">
+        <AppSidebar />
+        <div className="flex-1">
+          <Header />
+          <main className="p-4">{children}</main>
+        </div>
+      </div>
+    </SidebarProvider>
+  );
+}
